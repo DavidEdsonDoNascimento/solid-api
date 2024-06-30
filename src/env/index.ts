fd94b950d1aee02050b5869218b8bd2b1acc0760
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-30 18:45:55 -0300

    install and config Eslint and configure path with @

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
+import { z } from "zod";
 
 // validates the environment variables
+// z.coerce vai fazer com q variáveis de ambiente mesmo sendo informadas como string sejam convertidas para number
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
+});
 
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  const msg = "❌ Invalid environment variables";
+  console.log(msg, _env.error.format());
+  throw new Error(msg);
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import { app } from './app';
+import { app } from "@/app";
+import { env } from "@/env";
 
 // pass this host 0.0.0.0 to make it accessible to any other application
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}).then(() => {
-  console.log(`🚀 Server running on http://localhost:3333`);
-});
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 Server running on http://localhost:3333`);
+  });
