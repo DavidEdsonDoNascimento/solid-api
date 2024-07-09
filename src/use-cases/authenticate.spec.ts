56ba0b4205302d73f2d529ec062a1957483d57d0
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-08 21:58:13 -0300

    refactory in authenticate.spec and register.spec

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,24 +1,30 @@
-import { expect, describe, it } from "vitest";
+import { expect, describe, it, beforeEach } from "vitest";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { AuthenticateUseCase } from "./authenticate";
 import { hash } from "bcryptjs";
 import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { IUsersRepository } from "@/interfaces/user";
+
+let usersRepository: IUsersRepository;
+let sut: AuthenticateUseCase;
 
 describe("Authenticate Use Case", () => {
-  it("should be able to authenticate", async () => {
+  beforeEach(() => {
     // Be careful not to use another use case when testing a use case
     // as this violates the design pattern
     // That's why RegisterUseCase.execute was not called but IUserRepository.create,
     // so as not to run the risk of testing more than one use case
-    const usersRepository = new InMemoryUsersRepository();
+    usersRepository = new InMemoryUsersRepository();
+    // system under test S.U.T | global default generic variable
+    sut = new AuthenticateUseCase(usersRepository);
+  });
+
+  it("should be able to authenticate", async () => {
     usersRepository.create({
       name: "John Doe",
       email: "john.doe@example.com",
       password_hash: await hash("password123", 6),
     });
-
-    // system under test S.U.T | global default generic variable
-    const sut = new AuthenticateUseCase(usersRepository);
 
     const { user } = await sut.execute({
       email: "john.doe@example.com",
@@ -29,11 +35,6 @@ describe("Authenticate Use Case", () => {
   });
 
   it("should not be able to authenticate with wrong email", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-
-    // system under test S.U.T | global default generic variable
-    const sut = new AuthenticateUseCase(usersRepository);
-
     await expect(() =>
       sut.execute({
         email: "john.doe@example.com",
@@ -43,19 +44,11 @@ describe("Authenticate Use Case", () => {
   });
 
   it("should not be able to authenticate with wrong password", async () => {
-    // Be careful not to use another use case when testing a use case
-    // as this violates the design pattern
-    // That's why RegisterUseCase.execute was not called but IUserRepository.create,
-    // so as not to run the risk of testing more than one use case
-    const usersRepository = new InMemoryUsersRepository();
     usersRepository.create({
       name: "John Doe",
       email: "john.doe@example.com",
       password_hash: await hash("password123", 6),
     });
-
-    // system under test S.U.T | global default generic variable
-    const sut = new AuthenticateUseCase(usersRepository);
 
     await expect(() =>
       sut.execute({
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,15 +1,24 @@
-import { expect, describe, it } from "vitest";
+import { expect, describe, it, beforeEach } from "vitest";
 import { RegisterUseCase } from "./register";
 import { compare } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { IUsersRepository } from "@/interfaces/user";
+
+let userRepository: IUsersRepository;
+let sut: RegisterUseCase;
 
 describe("Register Use Case", () => {
-  it("should be able to register", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+  beforeEach(() => {
+    // creating a fictitious IUserRepository instance so that tests are not coupled to Prisma
+    // consequently without using the database, testing only the functionality and focusing on what really matters,
+    // which is ensuring that the functionality is doing what it should do
+    userRepository = new InMemoryUsersRepository();
+    sut = new RegisterUseCase(userRepository);
+  });
 
-    const { user } = await registerUseCase.execute({
+  it("should be able to register", async () => {
+    const { user } = await sut.execute({
       name: "John Doe",
       email: "john.doe@example.com",
       password: "password123",
@@ -19,14 +28,7 @@ describe("Register Use Case", () => {
   });
 
   it("should hash user password upon registration", async () => {
-    // creating a fictitious IUserRepository instance so that tests are not coupled to Prisma
-    // consequently without using the database, testing only the functionality and focusing on what really matters,
-    // which is ensuring that the functionality is doing what it should do
-    const userRepository = new InMemoryUsersRepository();
-
-    const registerUseCase = new RegisterUseCase(userRepository);
-
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: "John Doe",
       email: "john.doe@example.com",
       password: "password123",
@@ -41,23 +43,16 @@ describe("Register Use Case", () => {
   });
 
   it("should not be able to register with same email twice", async () => {
-    // creating a fictitious IUserRepository instance so that tests are not coupled to Prisma
-    // consequently without using the database, testing only the functionality and focusing on what really matters,
-    // which is ensuring that the functionality is doing what it should do
-    const userRepository = new InMemoryUsersRepository();
-
-    const registerUseCase = new RegisterUseCase(userRepository);
-
     const email = "john.doe@example.com";
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: "John Doe",
       email,
       password: "password123",
     });
 
     await expect(() =>
-      registerUseCase.execute({
+      sut.execute({
         name: "John Doe",
         email,
         password: "password123",
